fbe7bcedb18d6f885c9efd0fe2a3a599953cff73
Russell Gordon <rgordon@lcs.on.ca>
2022-11-17 11:41:35 -0500

    Initial Commit

---
+++ CustomShapesExamples/CustomShapesExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesExamplesApp.swift
+//  CustomShapesExamples
+//
+//  Created by Russell Gordon on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
